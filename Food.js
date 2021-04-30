500c5281a4460220c1761bdef4ead50ad3c92d2f
Simon5155Q <75984605+Simon5155Q@users.noreply.github.com>
2021-04-30 20:00:35 +0530

    Add files via upload

---
+++ Food.js
@@ -0,0 +1,69 @@
+class Food {
+    constructor(){
+    this.foodStock=0;
+    this.lastFed;
+    this.image=loadImage('Milk.png');
+    
+    }
+
+   updateFoodStock(foodStock){
+    this.foodStock=foodStock;
+   }
+
+   getFedTime(lastFed){
+     this.lastFed=lastFed;
+   }
+
+   deductFood(){
+     if(this.foodStock>0){
+      this.foodStock=this.foodStock-1;
+     } 
+  }
+    
+
+    getFoodStock(){
+      return this.foodStock;
+    }
+
+    deleteFood(){
+      var deleteFoods = foodObj.getFoodStock();
+      if(deleteFoods <= 0){
+        foodObj.updateFoodStock(deleteFoods.val * 0);
+        this.foodStock.update();
+      }
+      else{
+        foodObj.updateFoodStock(deleteFoods.val - 1);
+        this.foodStock.update();
+      }
+      
+    }
+
+    update(food){
+      database.ref("/").update({
+        foodStock:food
+      })
+  }
+
+     
+
+    display(){
+      var x=80,y=100;
+      
+      imageMode(CENTER);
+      image(this.image,720,220,70,70);
+      
+      if(this.foodStock!=0){
+        for(var i=0;i<this.foodStock;i++){
+          if(i%10==0){
+            x=80;
+            y=y+50;
+          }
+          image(this.image,x,y,50,50);
+          x=x+30;
+
+        }
+      }
+      
+      
+    }
+}
